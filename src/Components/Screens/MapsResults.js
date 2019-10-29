604186eace4c21c29e2f6a67470bfc86c22de5f2
usman.faisal60 <usman.faisal60@gmail.com>
2019-10-29 19:21:16 +0500

    =working till custom marker

--- src/Components/Screens/MapsResults.js
+++ src/Components/Screens/MapsResults.js
@@ -1,33 +1,70 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { View, Image, StyleSheet, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 
 class MapsResults extends React.Component {
+
+    state = {
+        initialRegion: {
+            latitude: 25.21,
+            longitude: 55.2708,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+        },
+
+        reigion: {
+
+        },
+
+    }
+
+    setReigion(reigion) {
+        this.setState({ reigion });
+    }
+
+    componentDidUpdate() {
+        console.log(this.state.reigion);
+    }
+
     render() {
 
         const {
             container,
-            mapStyle
+            mapStyle,
+            markerStyle
         } = Styles;
 
         return (
             <View style={container}>
-                <MapView style={mapStyle}/>
+                <MapView
+                    style={mapStyle}
+                    initialRegion={this.state.initialRegion}
+                    reigion={this.state.reigion}
+                    onRegionChange={this.setReigion.bind(this)}>
+                    <Marker coordinate={this.state.initialRegion}>
+                        <Image style={markerStyle} source={require('../../Assets/Icons/Host.png')} />
+                    </Marker>
+                </MapView>
             </View>
         )
     }
 }
-
 
 const Styles = StyleSheet.create({
     container: {
         width: '100%',
         height: '100%'
     },
+
     mapStyle: {
         width: '100%',
         height: '100%'
+    },
+
+    markerStyle: {
+        width: 30,
+        height: 30
     }
 });
 
